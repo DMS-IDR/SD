a6afdf4314da81729ee32578a087b1c27dae1227
Teobaldo Lunas <lunasteobaldo@gmail.com>
2026-01-08 17:04:02 -0300

    Se crea cliente api con funcionalidad interceptor para agregar token en los request

---
+++ frontend/src/shared/utils/clientApi.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+//TODO: Obtener url desde varibales de entorno
+
+const sdGestionApi = axios.create({
+    baseURL: import.meta.env.VITE_API_BACKEND_URL
+})
+
+// Interceptor de request
+
+sdGestionApi.interceptors.request.use(
+    (config) => {
+        // TODO: al momento de hacer login se debe solo guardar token en LS
+        const token = localStorage.getItem('token')
+
+        if(token) {
+            console.log(token)
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    }, 
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
+export default sdGestionApi;

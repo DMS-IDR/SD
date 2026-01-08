5a513923ac562e159ff5c5d40802f9696105e2b0
Teobaldo Lunas <lunasteobaldo@gmail.com>
2026-01-08 12:12:42 -0300

    Se crea nueva estructura de archivos para generar arquitectura por features

--- frontend/src/oldapp/components/CommissionView.jsx
+++ frontend/src/oldapp/components/CommissionView.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { supabase } from '../lib/supabase'
+import { supabase } from '../oldapp/lib/supabase'
 import jsPDF from 'jspdf'
 import autoTable from 'jspdf-autotable'
 
--- frontend/src/oldapp/components/Login.jsx
+++ frontend/src/oldapp/components/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { supabase } from '../lib/supabase'
+import { supabase } from '../oldapp/lib/supabase'
 
 export default function Login() {
     const [loading, setLoading] = useState(false)

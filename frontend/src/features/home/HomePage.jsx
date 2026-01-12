ac0e4b43b7d7d8fe4cbb63b64e5e15750d93ba7c
Teobaldo Lunas <lunasteobaldo@gmail.com>
2026-01-12 10:45:22 -0300

    Refactor HomePage component to utilize user privileges from useAuth hook, streamlining module visibility based on user permissions.

--- frontend/src/features/home/HomePage.jsx
+++ frontend/src/features/home/HomePage.jsx
@@ -1,19 +1,22 @@
 import { useNavigate } from "react-router-dom"
 import { HomeCard } from "./components"
 import { getVisibleModules } from "../../config"
+import { useAuth } from "../../app/context/AuthContext"
 
 
 export const HomePage = () => {
     const navigate = useNavigate()
 
-    const userPermissions = {
-        can_view_reports: false,
-        can_view_user_management: false,
-        can_view_closing_sales: false,
-        can_view_commission: false
-    };
+    const { user } = useAuth();
 
-    const availableModules = getVisibleModules(userPermissions);
+    // const userPermissions = {
+    //     can_view_reports: false,
+    //     can_view_user_management: false,
+    //     can_view_closing_sales: false,
+    //     can_view_commission: false
+    // };
+
+    const availableModules = getVisibleModules(user.privileges);
 
     return(
         <div className="max-w-7xl mx-auto p-4 sm:p-6">
